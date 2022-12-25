b9e7431068cc7c4085ada14cab940713c90e2d83
philanderson888 <philanderson888@hotmail.com>
2022-12-25 16:28:01 +0000

    add server2.js

---
+++ express/server2.js
@@ -0,0 +1,68 @@
+'use strict';
+const express = require('express');
+const path = require('path');
+const serverless = require('serverless-http');
+const app = express();
+const bodyParser = require('body-parser');
+const router = express.Router();
+
+router.get('/test3', (req, res) => {
+  console.log(`get '/test3' request received ...`)
+  res.writeHead(200, { 'Content-Type': 'text/html' });
+  res.write('<h1>Hello from Express.js!</h1>');
+  res.write('<p>This web server is running using Netlify Serverless Functions</p>');
+  res.write('<p>The live URL is <a href="https://netlify-express-serverless.netlify.app/.netlify/functions/server" target="_blank">https://netlify-express-serverless.netlify.app/.netlify/functions/server</a></p>');
+  res.write('<p>The instructions to build this came from <a href="https://github.com/philanderson888/netlify-express">https://github.com/philanderson888/netlify-express</a></p>');
+  res.write('<p>... which was a clone of <a href="https://github.com/neverendingqs/netlify-express">https://github.com/neverendingqs/netlify-express</a></p>');
+  res.write('<p>... code to build this server is below ...</p>');
+  res.write('<script src="https://gist.github.com/philanderson888/3b2c2c988c0425ef9360d145ff32966e.js"></script>');
+  res.end();
+});
+
+router.get('/another', (req, res) => {
+  console.log(`get() request received on /another path`)
+  res.json({ route: req.originalUrl });
+});
+
+router.post('/', (req, res) => res.json({ postBody: req.body }));
+
+app.use(bodyParser.json());
+
+app.use('/.netlify/functions/server', router);  // path must route to lambda
+
+app.use('/test2', (req, res) => {
+  console.log('request for /test2 received')
+  res.sendFile(path.join(__dirname, '../index.html'))
+});
+
+router.get('/', (req, res) => {
+  console.log(`get '/' request received ...`)
+  res.writeHead(200, { 'Content-Type': 'text/html' });
+  res.write('<h1>Hello from Express.js!</h1>');
+  res.write('<p>This web server is running using Netlify Serverless Functions</p>');
+  res.write('<p>The live URL is <a href="https://netlify-express-serverless.netlify.app/.netlify/functions/server" target="_blank">https://netlify-express-serverless.netlify.app/.netlify/functions/server</a></p>');
+  res.write('<p>The instructions to build this came from <a href="https://github.com/philanderson888/netlify-express">https://github.com/philanderson888/netlify-express</a></p>');
+  res.write('<p>... which was a clone of <a href="https://github.com/neverendingqs/netlify-express">https://github.com/neverendingqs/netlify-express</a></p>');
+  res.write('<p>... code to build this server is below ...</p>');
+  res.write('<script src="https://gist.github.com/philanderson888/3b2c2c988c0425ef9360d145ff32966e.js"></script>');
+  res.end();
+});
+
+app.use('/', (req, res) => {
+  console.log('app.use / request received - sending fixed file')
+  console.log(`get '/' request received ...`)
+  res.writeHead(200, { 'Content-Type': 'text/html' });
+  res.write('<h1>Hello from Express.js!</h1>');
+  res.write('<p>This web server is running using Netlify Serverless Functions</p>');
+  res.write('<p>The live URL is <a href="https://netlify-express-serverless.netlify.app/.netlify/functions/server" target="_blank">https://netlify-express-serverless.netlify.app/.netlify/functions/server</a></p>');
+  res.write('<p>The instructions to build this came from <a href="https://github.com/philanderson888/netlify-express">https://github.com/philanderson888/netlify-express</a></p>');
+  res.write('<p>... which was a clone of <a href="https://github.com/neverendingqs/netlify-express">https://github.com/neverendingqs/netlify-express</a></p>');
+  res.write('<p>... code to build this server is below ...</p>');
+  res.write('<script src="https://gist.github.com/philanderson888/3b2c2c988c0425ef9360d145ff32966e.js"></script>');
+  res.end();
+//  res.sendFile(path.join(__dirname, '../index.html'))
+});
+
+module.exports = app;
+
+module.exports.handler = serverless(app);
