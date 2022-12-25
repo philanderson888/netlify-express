27e6a3b2c54378598ea54263240804453a1bdbce
philanderson888 <philanderson888@hotmail.com>
2022-12-25 16:18:26 +0000

    add test3 route

--- express/server.js
+++ express/server.js
@@ -6,7 +6,18 @@ const app = express();
 const bodyParser = require('body-parser');
 const router = express.Router();
 
-
+router.get('/test3', (req, res) => {
+  console.log(`get '/test3' request received ...`)
+  res.writeHead(200, { 'Content-Type': 'text/html' });
+  res.write('<h1>Hello from Express.js!</h1>');
+  res.write('<p>This web server is running using Netlify Serverless Functions</p>');
+  res.write('<p>The live URL is <a href="https://netlify-express-serverless.netlify.app/.netlify/functions/server" target="_blank">https://netlify-express-serverless.netlify.app/.netlify/functions/server</a></p>');
+  res.write('<p>The instructions to build this came from <a href="https://github.com/philanderson888/netlify-express">https://github.com/philanderson888/netlify-express</a></p>');
+  res.write('<p>... which was a clone of <a href="https://github.com/neverendingqs/netlify-express">https://github.com/neverendingqs/netlify-express</a></p>');
+  res.write('<p>... code to build this server is below ...</p>');
+  res.write('<script src="https://gist.github.com/philanderson888/3b2c2c988c0425ef9360d145ff32966e.js"></script>');
+  res.end();
+});
 
 router.get('/another', (req, res) => {
   console.log(`get() request received on /another path`)
@@ -42,7 +53,6 @@ app.use('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../index.html'))
 });
 
-
-
 module.exports = app;
+
 module.exports.handler = serverless(app);
