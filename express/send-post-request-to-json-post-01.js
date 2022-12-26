21c448b6c318c97511520e3c895f522e322949e1
philanderson888 <philanderson888@hotmail.com>
2022-12-26 09:44:05 +0000

    add body parser

--- express/send-post-request-to-json-post-01.js
+++ express/send-post-request-to-json-post-01.js
@@ -3,6 +3,8 @@ const express = require('express');
 const app = express();
 const router = express.Router();
 const serverless = require('serverless-http');
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
 app.use('./netlify/functions/send-post-request-to-json-post-01.js', router);
 
 router.get('/', (req,res) => {
