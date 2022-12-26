2f73d2c63b62b47704f52fe5e9c1bfd9b40a8a73
philanderson888 <philanderson888@hotmail.com>
2022-12-26 05:15:27 +0000

    update server2

--- express/server2.js
+++ express/server2.js
@@ -6,9 +6,38 @@ const serverless = require('serverless-http');
 app.use('/.netlify/functions/server', router);  // path must route to lambda
 
 
-
 const path = require('path');
 const bodyParser = require('body-parser');
+
+
+app.use(bodyParser.json());
+
+
+app.use('/', (req, res) => {
+  console.log(`app.use('/') request received on /server2 ...`)
+  res.writeHead(200, { 'Content-Type': 'text/html' });
+  res.write(`<h1>Server2.js via app.use("/")</h1>`);
+  res.write('.. source ... https://github.com/philanderson888/netlify-express/blob/master/express/server2.js');
+  res.write('.. back .. <a target="_self" href="https://netlify-express-serverless.netlify.app/.netlify/functions/server>https://netlify-express-serverless.netlify.app/.netlify/functions/server</a>');
+  res.end();
+});
+
+router.get('/', (req, res) => {
+  console.log(`router.get(/) command received`)
+});
+
+module.exports = app;
+
+module.exports.handler = serverless(app);
+
+/*
+
+delete
+
+app.use('/test2', (req, res) => {
+  console.log('request for /test2 received')
+  res.sendFile(path.join(__dirname, '../index.html'))
+});
 
 router.get('/test3', (req, res) => {
   console.log(`get '/test3' request received ...`)
@@ -30,37 +59,6 @@ router.get('/another', (req, res) => {
 
 router.post('/', (req, res) => res.json({ postBody: req.body }));
 
-app.use(bodyParser.json());
+delete
 
-
-
-app.use('/test2', (req, res) => {
-  console.log('request for /test2 received')
-  res.sendFile(path.join(__dirname, '../index.html'))
-});
-
-app.use('/', (req, res) => {
-  console.log('app.use / request received on /server2')
-  res.writeHead(200, { 'Content-Type': 'text/html' });
-  res.write('<h1>Hello from Express.js!</h1>');
-  res.write('<p>This web server is running using Netlify Serverless Functions</p>');
-  res.write('<p>The live URL is <a href="https://netlify-express-serverless.netlify.app/.netlify/functions/server" target="_blank">https://netlify-express-serverless.netlify.app/.netlify/functions/server</a></p>');
-  res.write('<p>The instructions to build this came from <a href="https://github.com/philanderson888/netlify-express">https://github.com/philanderson888/netlify-express</a></p>');
-  res.write('<p>... which was a clone of <a href="https://github.com/neverendingqs/netlify-express">https://github.com/neverendingqs/netlify-express</a></p>');
-
-  
-  res.end();
-});
-
-router.get('/', (req, res) => {
-  console.log(`get '/' request received on /server2 ...`)
-  res.writeHead(200, { 'Content-Type': 'text/html' });
-  res.write('<h1>Server2.js via router.get("/")</h1>');
-  res.write('.. source ... https://github.com/philanderson888/netlify-express/blob/master/express/server2.js');
-  res.write('.. back .. <a target="_self" href="https://netlify-express-serverless.netlify.app/.netlify/functions/server>https://netlify-express-serverless.netlify.app/.netlify/functions/server</a>');
-  res.end();
-});
-
-module.exports = app;
-
-module.exports.handler = serverless(app);
+*/
