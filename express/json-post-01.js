6db1dddbe1063ab8dcd35954bc4d204f75b71ba2
philanderson888 <philanderson888@hotmail.com>
2022-12-26 06:30:02 +0000

    add json post with get(/) request

--- express/json-post-01.js
+++ express/json-post-01.js
@@ -1,9 +1,34 @@
 'use strict';
-const app = require('express')();
+const express = require('express');
+const app = express();
+const router = express.Router();
 const serverless = require('serverless-http');
+
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use('/.netlify/functions/server', router); 
 
 console.log(1);
 module.exports = app;
 console.log(2);
 module.exports.handler = serverless(app);
 console.log(3);
+
+router.get('/', (req, res) => {
+
+    console.log(`router.get('/') on json-post-01 .. `)
+
+  res.writeHead(200, { 'Content-Type': 'text/html' });
+
+  res.write(`<h1>router.get('/') on json-post-01 ..</h1>`);
+    
+  res.write(`<p>source .. <a href="https://github.com/philanderson888/netlify-express/blob/master/express/json-post-01.js">https://github.com/philanderson888/netlify-express/blob/master/express/json-post-01.js</a></p>`);
+
+  res.write(`<p>acknowledge <a target="_self" href="https://github.com/neverendingqs/netlify-express">https://github.com/neverendingqs/netlify-express</a></p>`);
+
+  res.end();
+});
+
+module.exports = app;
+
+module.exports.handler = serverless(app);
