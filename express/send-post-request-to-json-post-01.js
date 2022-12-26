25eb92414de84206ffce8d928f0cfbd955e448a5
philanderson888 <philanderson888@hotmail.com>
2022-12-26 10:20:36 +0000

    add in fetch code again

--- express/send-post-request-to-json-post-01.js
+++ express/send-post-request-to-json-post-01.js
@@ -11,23 +11,6 @@ console.log('something received')
 router.get('/', (req, res) => {
     console.log(`router.get('/') on json-send-post-request-to-json-post-01 .. `)
 
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-  
-    res.write(`<h1>router.get('/') on json-send-post-request-to-json-post-01 ..</h1>`);
-  
-    res.write(`<p>home .. router.get('/') <a target="_self" href="https://netlify-express-serverless.netlify.app/.netlify/functions/server">https://netlify-express-serverless.netlify.app/.netlify/functions/server</a></p>`);
-      
-    res.write(`<p>source .. <a href="https://github.com/philanderson888/netlify-express/blob/master/express/send-post-request-to-json-post-01.js">https://github.com/philanderson888/netlify-express/blob/master/express/send-post-request-to-json-post-01.js</a></p>`);
-  
-    res.write(`<p>The goal of this is to use nodejs to send a json POST fetch request to the given endpoind ... let's see if we can make it work ...`)
-
-    res.end();
-});
-module.exports = app;
-module.exports.handler = serverless(app);
-
-    /*
-  
     const url = `https://github.com/philanderson888/netlify-express/blob/master/express/send-post-request-to-json-post-01.js`;
 
     const headers = {
@@ -43,7 +26,20 @@ module.exports.handler = serverless(app);
         headers: headers,
         body: JSON.stringify(postData)
     })
-        .then(response => response.json())
-        .then(responseJson => console.log(responseJson));
-        
-    */
+    .then(response => response.json())
+    .then(responseJson => console.log(responseJson));
+
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+  
+    res.write(`<h1>router.get('/') on json-send-post-request-to-json-post-01 ..</h1>`);
+  
+    res.write(`<p>home .. router.get('/') <a target="_self" href="https://netlify-express-serverless.netlify.app/.netlify/functions/server">https://netlify-express-serverless.netlify.app/.netlify/functions/server</a></p>`);
+      
+    res.write(`<p>source .. <a href="https://github.com/philanderson888/netlify-express/blob/master/express/send-post-request-to-json-post-01.js">https://github.com/philanderson888/netlify-express/blob/master/express/send-post-request-to-json-post-01.js</a></p>`);
+  
+    res.write(`<p>The goal of this is to use nodejs to send a json POST fetch request to the given endpoind ... let's see if we can make it work ...`)
+
+    res.end();
+});
+module.exports = app;
+module.exports.handler = serverless(app);
