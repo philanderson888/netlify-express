77ba273325435636d71041eb8e5eeae8a8639e7b
philanderson888 <philanderson888@hotmail.com>
2022-12-26 05:17:55 +0000

    update server.js

--- express/server.js
+++ express/server.js
@@ -32,21 +32,23 @@ router.get('/', (req, res) => {
   res.write('<p>server3 at <a href="https://netlify-express-serverless.netlify.app/.netlify/functions/server3" target="_blank">https://netlify-express-serverless.netlify.app/.netlify/functions/server3</a></p>');
   res.write('<p>server4 at <a href="https://netlify-express-serverless.netlify.app/.netlify/functions/server4" target="_blank">https://netlify-express-serverless.netlify.app/.netlify/functions/server4</a></p>');
   
-  res.write('<p>json01 .. return empty json ..<a href="https://netlify-express-serverless.netlify.app/.netlify/functions/json01" target="_self">https://netlify-express-serverless.netlify.app/.netlify/functions/json01</a></p>');
+  res.write('<p>json01 .. return json ..<a href="https://netlify-express-serverless.netlify.app/.netlify/functions/json01" target="_self">https://netlify-express-serverless.netlify.app/.netlify/functions/json01</a></p>');
   
   res.write('<p>json02 .. return json <a href="https://netlify-express-serverless.netlify.app/.netlify/functions/json02" target="_self">https://netlify-express-serverless.netlify.app/.netlify/functions/json02</a></p>');
   
   res.write('<p>json-with-params at <a href="https://netlify-express-serverless.netlify.app/.netlify/functions/json-with-params?name=phil&dob=210989" target="_self">https://netlify-express-serverless.netlify.app/.netlify/functions/json-with-params?name=phil&dob=210989</a></p>');
   
   res.write('<p>json-post-01 at <a href="https://netlify-express-serverless.netlify.app/.netlify/functions/json-post-01" target="_blank">https://netlify-express-serverless.netlify.app/.netlify/functions/json-post-01</a></p>');
+
   res.write('<p>The instructions to build this came from <a href="https://github.com/philanderson888/netlify-express">https://github.com/philanderson888/netlify-express</a></p>');
+
   res.write('<p>... which was a clone of <a href="https://github.com/neverendingqs/netlify-express">https://github.com/neverendingqs/netlify-express</a></p>');
 
-  res.write('.. source ... <a href="https://github.com/philanderson888/netlify-express/blob/master/express/server.js" target="_self">https://github.com/philanderson888/netlify-express/blob/master/express/server.js</a>');
+  res.write('<p>.. source ... <a href="https://github.com/philanderson888/netlify-express/blob/master/express/server.js" target="_self">https://github.com/philanderson888/netlify-express/blob/master/express/server.js</a></p>');
   
-  res.write('.. home .. <a target="_self" href="https://netlify-express-serverless.netlify.app/.netlify/functions/server">https://netlify-express-serverless.netlify.app/.netlify/functions/server</a>');
+  res.write('<p>.. home .. <a target="_self" href="https://netlify-express-serverless.netlify.app/.netlify/functions/server">https://netlify-express-serverless.netlify.app/.netlify/functions/server</a></p>');
 
-  res.write('<p>... code to build this server is below ...</p>');
+  res.write('<p>... source gist .. </p>');
   res.write('<script src="https://gist.github.com/philanderson888/3b2c2c988c0425ef9360d145ff32966e.js"></script>');
   res.end();
 });
